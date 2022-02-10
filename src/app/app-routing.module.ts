390d21777fbdea0d2a21cc6667985f3b00e6e285
Julio Berina <julioberina@gmail.com>
2022-02-09 23:15:30 -0800

    Create 4 modules for nav links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'destination', loadChildren: () => import('./destination/destination.module').then(m => m.DestinationModule) },
+  { path: 'crew', loadChildren: () => import('./crew/crew.module').then(m => m.CrewModule) },
+  { path: 'technology', loadChildren: () => import('./technology/technology.module').then(m => m.TechnologyModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
