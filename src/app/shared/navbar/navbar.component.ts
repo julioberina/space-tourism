7b4ee168f3d5a24d8bfc512a500f4de1370239b0
Julio Berina <julioberina@gmail.com>
2022-02-12 22:10:23 -0800

    Finish mobile navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  public isOpened = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  open() {
+    this.isOpened = true;
+    (document.getElementById('m-nav') as HTMLDivElement)?.classList?.add('nav-open');
+  }
+
+  close() {
+    this.isOpened = false;
+    (document.getElementById('m-nav') as HTMLDivElement)?.classList?.remove('nav-open');
+  }
 }
