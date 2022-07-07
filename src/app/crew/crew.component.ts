f8c7b8609695b2961ee83b9e2e529a5891654166
Julio Berina <julioberina@gmail.com>
2022-07-06 23:10:15 -0700

    Work on crew page

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-crew',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrewComponent implements OnInit {
 
-  constructor() { }
+  private data: any;
+  public crew: any;
+  public photoSrc: any;
+
+  public name = '';
+  public role = '';
+  public bio = '';
+
+  constructor(private appService: AppService) { }
 
   ngOnInit(): void {
+    this.data = this.appService.getData().default;
+    this.crew = this.data.crew;
   }
 
+  ngAfterViewInit(): void {
+    this.select(0);
+  }
+
+  select(index: number) {
+    this.photoSrc = '../.' + this.crew[index].images.png;
+    this.name = this.crew[index].name;
+    this.role = this.crew[index].role;
+    this.bio = this.crew[index].bio;
+  }
 }
--- src/app/crew/crew.module.ts
+++ src/app/crew/crew.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { CrewRoutingModule } from './crew-routing.module';
 import { CrewComponent } from './crew.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { CrewComponent } from './crew.component';
   ],
   imports: [
     CommonModule,
-    CrewRoutingModule
+    CrewRoutingModule,
+    SharedModule
   ]
 })
 export class CrewModule { }
